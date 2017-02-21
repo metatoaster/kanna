96f07e033df99bf1287078b3b9ae81be77c49cd4
Tommy Yu <y@metatoaster.com>
2017-02-21 20:37:23 +1300

    Refactor the logic a bit
    - Split off the loader so it can be reused
    - Introduce argparser for dealing with other arguments
    - Also provide those arguments to the runtime.

--- asciibleed.py
+++ asciibleed.py
@@ -3,11 +3,14 @@ import os
 import re
 import sys
 
+from argparse import ArgumentParser
 from curses import setupterm
 from curses import tigetnum
 from curses import tigetstr
 from curses import tparm
 from time import sleep
+
+default_encoding = sys.getdefaultencoding()
 
 
 def get_width():
@@ -49,39 +52,41 @@ def bleed_text(text, count, offset, startrow, period):
         sleep(period)
 
 
-def main(filename, count=23, offset=23, startrow=10, period=0.084):
+def loader(filename):
+    if not os.path.exists(filename):
+        raise ValueError("'%s' not found\n" % filename)
+
+    if not os.path.isfile(filename):
+        raise ValueError("'%s' is not a file\n" % filename)
+
+    try:
+        with open(filename, 'rb') as fd:
+            raw = fd.read()
+    except (IOError, OSError):
+        raise ValueError("error reading '%s'\n" % filename)
+
+    encodings = ['utf-8', 'ascii', default_encoding]
+    for c in encodings:
+        try:
+            return raw.decode('utf-8')
+        except UnicodeDecodeError:
+            continue
+
+    raise ValueError("error decoding '%s' with encodings: %s\n" % (
+        filename, ', '.join(set(encodings))))
+
+
+def simple(filename, count=23, offset=23, startrow=10, period=0.084):
     """
     count - how many times to "bleed" (Kobayashi OP Kanna default = 23)
     offset - how many columns to offset
     startrow - rows to offset from the top
     """
 
-    if not os.path.exists(filename):
-        sys.stderr.write("'%s' not found\n" % filename)
-        return 1
-
-    if not os.path.isfile(filename):
-        sys.stderr.write("'%s' is not a file\n" % filename)
-        return 1
-
     try:
-        with open(filename, 'rb') as fd:
-            raw = fd.read()
-    except (IOError, OSError):
-        sys.stderr.write("error reading '%s'\n" % filename)
-        return 1
-
-    encodings = ['utf-8', 'ascii', sys.getdefaultencoding()]
-    text = None
-    for c in encodings:
-        try:
-            text = raw.decode('utf-8')
-        except UnicodeDecodeError:
-            continue
-
-    if text is None:
-        sys.stderr.write("error decoding '%s' with encodings: %s\n" % (
-            filename, ', '.join(set(encodings))))
+        text = loader(filename)
+    except ValueError as e:
+        sys.stderr.write(str(e))
         return 1
 
     bleed_text(text, count, offset, startrow, period)
@@ -89,7 +94,23 @@ def main(filename, count=23, offset=23, startrow=10, period=0.084):
 
 
 if __name__ == '__main__':
-    if not sys.argv[1:]:
-        sys.stderr.write('usage: %s <textfile>\n' % sys.argv[0])
-        sys.exit(1)
-    sys.exit(main(sys.argv[1]))
+    argparser = ArgumentParser()
+    argparser.add_argument('filename', action='store')
+    argparser.add_argument(
+        '-r', '--startrow', action='store', default=10, type=int,
+        help="number of rows to offset from top")
+    argparser.add_argument(
+        '-o', '--offset', action='store', default=23, type=int,
+        help="number of columns for horizontal offsets")
+    argparser.add_argument(
+        '--period', action='store', default=0.083, type=float,
+        help="period (timeout per frame for simple (not script) mode)")
+
+    args = argparser.parse_args(sys.argv[1:])
+
+    sys.exit(simple(
+        args.filename,
+        offset=args.offset,
+        startrow=args.startrow,
+        period=args.period,
+    ))
